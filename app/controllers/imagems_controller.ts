85edef6593286492354d1e7d7e94d0fa33ff4620
Daniella Nascimento <daniellateun@gmail.com>
2024-06-05 21:37:05 -0300

    upload e download de imagens

--- app/controllers/imagems_controller.ts
+++ app/controllers/imagems_controller.ts
@@ -1,6 +1,8 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 import Imagem from "#models/imagem";
+import app from '@adonisjs/core/services/app';
+import { normalize } from 'path';
 
 export default class ImagemsController {
   async index({request}: HttpContext){
@@ -9,16 +11,16 @@ export default class ImagemsController {
     return await Imagem.query().paginate(page,perPage)
   }
 
-  async show({params}: HttpContext){
-    return await Imagem.query().where('id', params.id)
-    .preload('veiculo')
-    .firstOrFail()
-  }
+  // async show({params}: HttpContext){
+  //   return await Imagem.query().where('id', params.id)
+  //   .preload('veiculo')
+  //   .firstOrFail()
+  // }
 
-  async store({request}: HttpContext){
-    const dados = request.only(['nome', 'imagem', 'tipo', 'veiculoId'])
-    return await Imagem.create(dados)
-  }
+  // async store({request}: HttpContext){
+  //   const dados = request.only(['nome', 'imagem', 'tipo', 'veiculoId'])
+  //   return await Imagem.create(dados)
+  // }
 
   async update({params, request}: HttpContext){
     const imagem = await Imagem.findOrFail(params.id)
@@ -32,5 +34,39 @@ export default class ImagemsController {
       await imagem.delete()
       return {'imagem deletado:': imagem}
 
+  }
+
+  //Upload
+  async store({request, response}: HttpContext) {
+    const img = request.file('  ', {
+      size: '2mb',
+      extnames: ['jpg', 'png', 'jpeg']
+    })
+
+    if (!img?.isValid) {
+      return response.badRequest({
+        errors: img?.errors
+      })
+    }
+    const nome = img.clientName.split('.')[0];
+    await img.move(app.makePath('uploads'), {
+      name: `${nome}.${img.extname}`
+    })
+    const data = {
+      nome: nome,
+      tipo: img.extname,
+      imagem: img.clientName
+    }
+    return await Imagem.create(data)
+  }
+  // Download
+  async show({params, response}: HttpContext) {
+
+    const dados = await Imagem.findOrFail(params.id)
+    const nomeImagem = normalize(dados.imagem)
+
+    const caminhoImagem = app.makePath('uploads', nomeImagem)
+
+    return response.download(caminhoImagem)
   }
 }
